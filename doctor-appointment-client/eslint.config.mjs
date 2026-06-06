be9904f7793036af10fcc0794aa7925905b6efb1
fahim <fahimahamedweb@gmail.com>
2026-06-07 04:47:34 +0600

    Added soem chanegs

---
+++ doctor-appointment-client/eslint.config.mjs
@@ -0,0 +1,16 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  {
+    rules: {
+      "react-hooks/immutability": "off",
+      "react-hooks/preserve-manual-memoization": "off",
+      "react-hooks/set-state-in-effect": "off",
+    },
+  },
+];
+
+export default eslintConfig;
--- doctor-appointment-client/next.config.mjs
+++ doctor-appointment-client/next.config.mjs
@@ -1,14 +1,20 @@
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-  
-    images: {
-        remotePatterns: [
-          {
-            hostname: '*',
-          },
-        ],
+  turbopack: {
+    root: __dirname,
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "*",
       },
+    ],
+  },
 };
 
 export default nextConfig;
